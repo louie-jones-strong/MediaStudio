0f362a5258e7a04c19fd61a2f0e06cb6fd88c17a
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2022-11-08 23:01:19 +0000

    fix picking brushes

--- Code/Tools/Brushes/BrushManger.js
+++ Code/Tools/Brushes/BrushManger.js
@@ -4,27 +4,29 @@ class BrushManger extends Selectable
 	constructor()
 	{
 		super();
-		this.Brushes = [];
+		this.Brushes = {};
 		this.SelectedBrush = 0;
+		this.BrushesAdded = 0;
 
-		this.Brushes.push(new SolidCircleBrush());
-		this.Brushes.push(new CustomBrush(0, "assets/Brushes/Custom1.png"));
-		this.Brushes.push(new CustomBrush(1, "assets/Brushes/SmoothCircle.png"));
+		this.AddBrush(new SolidCircleBrush());
+		this.AddBrush(new CustomBrush(0, "assets/Brushes/Custom1.png"));
+		this.AddBrush(new CustomBrush(1, "assets/Brushes/SmoothCircle.png"));
 	}
 
-	SelectBrush(index)
+	AddBrush(brush)
 	{
-		this.SelectedBrush = 0;
+		this.Brushes[this.BrushesAdded] = brush;
+
+		this.BrushesAdded += 1;
 	}
 
 	DrawOptions()
 	{
 		var self = this
-		for (let i = 0; i < this.Brushes.length; i++)
+		for (const key in this.Brushes)
 		{
-			const item = this.Brushes[i];
-			var index = i
-			this.AddOption(item.Id, item.Icon, function(){self.SelectedBrush = index})
+			const item = this.Brushes[key];
+			this.AddOption(item.Id, item.Icon, function(){self.SelectedBrush = key;})
 		}
 
 		this.SelectOption(this.Brushes[this.SelectedBrush].Id);
